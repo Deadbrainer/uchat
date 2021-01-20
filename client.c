8f799eab02fdd0238ba0ffcba10518809baa688d
Roman Luchnoi <rluchnoi@c1r10p5.khpi.ucode-connect.study>
2021-01-20 16:52:57 +0200

    client change

--- client.c
+++ client.c
@@ -12,9 +12,7 @@
 #define LENGTH 2048
 
 // Global variables
-volatile sig_atomic_t flag = 0;
-int sockfd = 0;
-char name[32];
+
 
 void str_overwrite_stdout() {
   printf("%s", "> ");
@@ -31,11 +29,11 @@ void str_trim_lf (char* arr, int length) {
   }
 }
 
-void catch_ctrl_c_and_exit(int sig) {
-    flag = 1;
-}
+// void catch_ctrl_c_and_exit(int sig, volatile sig_atomic_t flag) {
+//     flag = 1;
+// }
 
-void send_msg_handler() {
+void send_msg_handler(int sockfd, char* name) {
   char message[LENGTH] = {};
 	char buffer[LENGTH + 32] = {};
 
@@ -54,10 +52,11 @@ void send_msg_handler() {
 		bzero(message, LENGTH);
     bzero(buffer, LENGTH + 32);
   }
-  catch_ctrl_c_and_exit(2);
+  //catch_ctrl_c_and_exit(2, flag);
 }
 
-void recv_msg_handler() {
+
+void recv_msg_handler(int sockfd) {
 	char message[LENGTH] = {};
   while (1) {
 		int receive = recv(sockfd, message, LENGTH, 0);
@@ -74,6 +73,14 @@ void recv_msg_handler() {
 }
 
 int main(int argc, char **argv){
+
+	//volatile sig_atomic_t flag = 0;
+	int sockfd = 0;
+	char name[32];
+
+	char password[32];
+
+
 	if(argc != 2){
 		printf("Usage: %s <port>\n", argv[0]);
 		return EXIT_FAILURE;
@@ -82,11 +89,15 @@ int main(int argc, char **argv){
 	char *ip = "127.0.0.1";
 	int port = atoi(argv[1]);
 
-	signal(SIGINT, catch_ctrl_c_and_exit);
+	//signal(SIGINT, catch_ctrl_c_and_exit);
 
 	printf("Please enter your name: ");
-  fgets(name, 32, stdin);
-  str_trim_lf(name, strlen(name));
+  	fgets(name, 32, stdin);
+  	str_trim_lf(name, strlen(name));
+
+	printf("Please enter your password: ");
+  	fgets(password, 32, stdin);
+  	str_trim_lf(name, strlen(name));
 
 
 	if (strlen(name) > 32 || strlen(name) < 2){
@@ -94,18 +105,23 @@ int main(int argc, char **argv){
 		return EXIT_FAILURE;
 	}
 
+	if (strlen(password) > 32 || strlen(password) < 2){
+		printf("Password must be less than 30 and more than 2 characters.\n");
+		return EXIT_FAILURE;
+	}
+
 	struct sockaddr_in server_addr;
 
 	/* Socket settings */
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = inet_addr(ip);
-  server_addr.sin_port = htons(port);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(ip);
+    server_addr.sin_port = htons(port);
 
 
-  // Connect to Server
-  int err = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-  if (err == -1) {
+    // Connect to Server
+    int err = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    if (err == -1) {
 		printf("ERROR: connect\n");
 		return EXIT_FAILURE;
 	}
@@ -113,26 +129,29 @@ int main(int argc, char **argv){
 	// Send name
 	send(sockfd, name, 32, 0);
 
+	//Send password
+	send(sockfd, password, 32, 0);
+
 	printf("=== WELCOME TO THE CHATROOM ===\n");
 
 	pthread_t send_msg_thread;
-  if(pthread_create(&send_msg_thread, NULL, (void *) send_msg_handler, NULL) != 0){
+  	if (pthread_create(&send_msg_thread, NULL, (void *) send_msg_handler, NULL) != 0) {
 		printf("ERROR: pthread\n");
-    return EXIT_FAILURE;
+    	return EXIT_FAILURE;
 	}
 
 	pthread_t recv_msg_thread;
-  if(pthread_create(&recv_msg_thread, NULL, (void *) recv_msg_handler, NULL) != 0){
+  	if (pthread_create(&recv_msg_thread, NULL, (void *) recv_msg_handler, NULL) != 0) {
 		printf("ERROR: pthread\n");
 		return EXIT_FAILURE;
 	}
 
-	while (1){
-		if(flag){
-			printf("\nBye\n");
-			break;
-    }
-	}
+	// while (1){
+	// 	if(flag){
+	// 		printf("\nBye\n");
+	// 		break;
+    // }
+	// }
 
 	close(sockfd);
 
