ec7e10d8dd36454f2395b4fa37e50da5b382ec3b
Artem <cnfhrhfan67@gmail.com>
2021-02-03 16:34:39 +0200

    here some progress

--- src/main_menu.c
+++ src/main_menu.c
@@ -2,24 +2,38 @@
 
 GtkWidget *textArea;
 
-void text_add()
+void get_text_entry(GtkWidget **textEntry, bool check)
 {
-    
+    static GtkWidget *t;
+    if (check)
+    {
+        t = *textEntry;
+    }
+    else
+    {
+        *textEntry = t;
+    }
 }
 
-int on_key_press(GtkWidget *widget, GdkEventKey *event, GtkTextBuffer *buffer/*gpointer user_data*/)
+int on_key_press(GtkWidget *widget, GdkEventKey *event, GtkTextBuffer *buffer /*gpointer user_data*/)
 {
-    if(event->keyval == GDK_KEY_KP_Enter || event->keyval == GDK_KEY_Return)
-    {
+    if (event->keyval == GDK_KEY_KP_Enter || event->keyval == GDK_KEY_Return)
+    { 
         GtkTextIter iter;
         GtkTextIter end;
         GtkTextMark *cursor;
+        GtkWidget *textEntry;
+        get_text_entry(&textEntry, 0);
+
+        const gchar *text = gtk_entry_get_text(GTK_ENTRY(textEntry));
+
         cursor = gtk_text_buffer_get_mark (buffer, "insert");
         gtk_text_buffer_get_iter_at_mark (buffer, &iter, cursor);
-        gtk_text_iter_set_line_offset (&iter, 0);
-        gtk_text_buffer_insert (buffer, &iter, "<li>", -1);
-        gtk_text_iter_forward_to_line_end (&iter);
-        gtk_text_buffer_insert (buffer, &iter, "</li>", -1);
+        gtk_text_iter_forward_to_end(&iter);
+        gtk_text_buffer_place_cursor (buffer, &iter);
+        gtk_text_buffer_insert (buffer, &iter, text, -1);
+        gtk_text_buffer_insert (buffer, &iter, "\n\n", -1);
+        gtk_entry_set_text(GTK_ENTRY(textEntry), "");
     }
     return false;
 }
@@ -28,14 +42,12 @@ void main_menu()
 {
     GtkWidget *scrolledwindow = gtk_scrolled_window_new(NULL, NULL);
     GtkWidget *textEntry = gtk_entry_new();
-    gtk_entry_set_text(GTK_ENTRY(textEntry), "void");
+    get_text_entry(&textEntry, 1);
     textArea = gtk_text_view_new();
-
     GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
-    GtkTextBuffer *buffer;
-    buffer = gtk_text_view_get_buffer (GTK_TEXT_VIEW (textArea));
-    gtk_text_buffer_set_text (buffer, "Item1\nItem2\nItem3", -1);
 
+    GtkTextBuffer *buffer;
+    buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(textArea));
 
     //textview parametres
     gtk_text_view_set_editable(GTK_TEXT_VIEW(textArea), false);
@@ -47,7 +59,7 @@ void main_menu()
 
     g_signal_connect(G_OBJECT(main_window), "delete-event", G_CALLBACK(closeApp), NULL);
     //g_signal_connect_swapped(G_OBJECT(textEntry), "key-press-event", G_CALLBACK(on_key_press), textEntry);
-    g_signal_connect(G_OBJECT(main_window), "key-press-event", G_CALLBACK(on_key_press), buffer);
+    g_signal_connect(G_OBJECT(textEntry), "key-press-event", G_CALLBACK(on_key_press), buffer);
 
     gtk_container_add(GTK_CONTAINER(scrolledwindow), textArea);
     gtk_box_pack_start(GTK_BOX(vbox), scrolledwindow, true, true, 5);
